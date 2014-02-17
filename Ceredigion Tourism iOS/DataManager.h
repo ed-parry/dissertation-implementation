1165f884770951406b61b45cfb0919371112bd18
Ed Parry <me@edparry.com>
2014-02-17 15:24:33 +0000

    Building the DataManager class to be able to get a CSV file from a URL.

---
+++ Ceredigion Tourism iOS/DataManager.h
@@ -0,0 +1,16 @@
+//
+//  DataManager.h
+//  Ceredigion Tourism iOS
+//
+//  Created by Ed Parry on 17/02/2014.
+//  Copyright (c) 2014 Aberystwyth University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataManager : NSObject
+
+- (void)saveDataFromURL:(NSString *)url;
+
+
+@end
---
+++ Ceredigion Tourism iOS/DataManager.m
@@ -0,0 +1,70 @@
+//
+//  DataManager.m
+//  Ceredigion Tourism iOS
+//
+//  Created by Ed Parry on 17/02/2014.
+//  Copyright (c) 2014 Aberystwyth University. All rights reserved.
+//
+
+#import "DataManager.h"
+
+@interface DataManager ()
+- (NSString *)getTodaysDate;
+- (void)saveDataToCoreData:(NSString *)fullFilePath;
+
+@property NSMutableData *dataReceived;
+@end
+
+@implementation DataManager
+
+- (void)saveDataToCoreData:(NSString *)fullFilePath;
+{
+    
+}
+
+- (void)saveDataFromURL:(NSString *)urlString
+{
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [NSURLConnection connectionWithRequest:request delegate:self];
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    // if we have all of the data from the URL, save it to file
+    NSString *documentFolder = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    
+    // Append today's date to the file name, so we know how old the data is in the future.
+    NSString *fullFileName = [NSString stringWithFormat:@"attractions-data-%@.csv", [self getTodaysDate]];
+    
+    NSString *fullFilePath = [NSString stringWithFormat:@"%@/%@", documentFolder, fullFileName];
+    
+    [_dataReceived writeToFile:fullFilePath atomically:YES];
+    
+    // We now have a local CSV file to use, let's store it in Core Data
+    [self saveDataToCoreData:fullFilePath];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+    if (_dataReceived){
+        [_dataReceived appendData:data];
+    }
+    else{
+        // if there isn't any data already here, make a new object
+        // with this first set of data.
+        _dataReceived = [[NSMutableData alloc] initWithData:data];
+    }
+}
+
+- (NSString *)getTodaysDate
+{
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"yyyy-MM-dd"];
+    
+    NSString *date = [dateFormatter stringFromDate:[NSDate date]];
+    
+    return date;
+}
+
+@end
