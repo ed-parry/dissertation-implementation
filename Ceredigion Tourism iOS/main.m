fc1b1bcd7f562a455f49ead14cd4d4aead39c5f9
Ed Parry <me@edparry.com>
2014-02-17 14:26:53 +0000

    Initial commit from local git

---
+++ Ceredigion Tourism iOS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ceredigion Tourism iOS
+//
+//  Created by Ed Parry on 17/02/2014.
+//  Copyright (c) 2014 Aberystwyth University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Ceredigion Tourism iOSTests/Ceredigion_Tourism_iOSTests.m
@@ -0,0 +1,34 @@
+//
+//  Ceredigion_Tourism_iOSTests.m
+//  Ceredigion Tourism iOSTests
+//
+//  Created by Ed Parry on 17/02/2014.
+//  Copyright (c) 2014 Aberystwyth University. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Ceredigion_Tourism_iOSTests : XCTestCase
+
+@end
+
+@implementation Ceredigion_Tourism_iOSTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
