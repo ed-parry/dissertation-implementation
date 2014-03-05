a2e029e8d28dc65cce3f87c4e32e7bf19b69cb02
Ed Parry <me@edparry.com>
2014-03-05 17:27:18 +0000

    Implemented the ability to have a radius show, and to only show the markers within that radius on the map view.

--- Ceredigion Tourism iOS/MapViewController.m
+++ Ceredigion Tourism iOS/MapViewController.m
@@ -15,18 +15,24 @@
 
 @interface MapViewController () <GMSMapViewDelegate>
 - (void)buildMapMarkers;
+
+// Core Features
 @property CLLocationManager *locationManager;
-
 @property GMSMapView *mapView;
-
 @property NSArray *attractionPositions;
 
+// Loading
+@property (strong, nonatomic) IBOutlet UIActivityIndicatorView *mapLoadSpinner;
+
+// Markers
 @property GMSMarker *tappedMarker;
 @property (strong, nonatomic) IBOutlet UIView *customMapMarker;
 @property (strong, nonatomic) IBOutlet UILabel *customMarkerName;
 @property (strong, nonatomic) IBOutlet UILabel *customMarkerGroup;
 
-@property (strong, nonatomic) IBOutlet UIActivityIndicatorView *mapLoadSpinner;
+// Radius
+@property double currentRadiusInMeters;
+@property CLLocationCoordinate2D currentRadiusCenter;
 @end
 
 @implementation MapViewController
@@ -105,10 +111,11 @@
     CoreDataManager *dataManager = [[CoreDataManager alloc] init];
     _attractionPositions = [dataManager getAllAttractionPositions];
 
+    [self changeMapRadiusView:10 withCenter:_locationManager.location.coordinate];
     [self buildMapMarkers];
     
     // Map Radius Testing
-    [self changeMapRadiusView:10];
+
     
     _mapView.delegate = (id)self;
 }
@@ -119,20 +126,24 @@
     self.view = _mapView;
 }
 
-- (void)changeMapRadiusView:(double)miles
+- (void)changeMapRadiusView:(double)miles withCenter:(CLLocationCoordinate2D)centerCoordinates
 {
-    double latitude = _locationManager.location.coordinate.latitude;
-    double longitude = _locationManager.location.coordinate.longitude;
+    _currentRadiusCenter = centerCoordinates;
+    
+    double latitude = centerCoordinates.latitude;
+    double longitude = centerCoordinates.longitude;
     
     double meters = [self changeMilesToMeters:miles];
+    _currentRadiusInMeters = meters;
     
     CLLocationCoordinate2D circleCenter = CLLocationCoordinate2DMake(latitude, longitude);
-    GMSCircle *circ = [GMSCircle circleWithPosition:circleCenter
+    GMSCircle *circleRadius = [GMSCircle circleWithPosition:circleCenter
                                              radius:meters];
-    circ.fillColor = [UIColor colorWithRed:0 green:0 blue:0.25 alpha:0.10];
-    circ.strokeColor = [UIColor blueColor];
-    circ.strokeWidth = 2;
-    circ.map = _mapView;
+    circleRadius.fillColor = [UIColor colorWithRed:0 green:0 blue:0.25 alpha:0.10];
+    circleRadius.strokeColor = [UIColor blueColor];
+    circleRadius.strokeWidth = 2;
+    circleRadius.map = _mapView;
+    
     [self putMapOnView];
 }
 
@@ -151,21 +162,57 @@
 
 - (void)buildMapMarkers{
     for(Attraction *currentAttraction in _attractionPositions){
-            GMSMarker *attractionMarker = [[GMSMarker alloc] init];
+
+        CLLocationCoordinate2D attractionCoordinates;
+        attractionCoordinates.latitude = [currentAttraction.latitude doubleValue];
+        attractionCoordinates.longitude = [currentAttraction.longitude doubleValue];
+        
+        if([self isCoordinatesWithinRadius:attractionCoordinates])
+        {
             double attractionLat = [currentAttraction.latitude doubleValue];
             double attractionLong = [currentAttraction.longitude doubleValue];
-            
+            GMSMarker *attractionMarker = [[GMSMarker alloc] init];
             attractionMarker.position = CLLocationCoordinate2DMake(attractionLat, attractionLong);
             attractionMarker.title = currentAttraction.name;
             attractionMarker.snippet = currentAttraction.group;
-//            attractionMarker.infoWindowAnchor = CGPointMake(0.44f, 0.45f);
-        
+            
             // Make a new Attraciton object to grab the correct group colour.
             Attraction *colourAttractionObj = [[Attraction alloc] init];
             attractionMarker.icon = [GMSMarker markerImageWithColor:[colourAttractionObj getAttractionGroupColor:currentAttraction.group]];
             
             attractionMarker.map = _mapView;
+        }
+        else{
+            // The marker is outside of the current radius, so we shouldn't show it.
+        }
     }
+}
+
+- (BOOL)isCoordinatesWithinRadius:(CLLocationCoordinate2D)coordinates
+{
+    CLLocationCoordinate2D circleCenter = _currentRadiusCenter;
+    double circleRadius = _currentRadiusInMeters;
+    
+    double distanceFromMiddle = [self getDistanceInMetersFrom:circleCenter to:coordinates];
+    
+    if(distanceFromMiddle <= circleRadius){
+        // It's inside the radius
+        return YES;
+    }
+    else{
+        // It's outside of the radius.
+        return NO;
+    }
+}
+
+- (double)getDistanceInMetersFrom:(CLLocationCoordinate2D)firstPoint to:(CLLocationCoordinate2D)secondPoint
+{
+    CLLocation *firstLocation = [[CLLocation alloc] initWithLatitude:firstPoint.latitude longitude:firstPoint.longitude];
+    CLLocation *secondLocation = [[CLLocation alloc] initWithLatitude:secondPoint.latitude longitude:secondPoint.longitude];
+    
+    double distanceInMeters = [firstLocation distanceFromLocation:secondLocation];
+    
+    return distanceInMeters;
 }
 
 // Develop a custom view for the marker overlays.
@@ -177,6 +224,10 @@
     _customMarkerGroup.text = marker.snippet;
 
     return _customMapMarker;
+    
+
+
+    
 }
 
 - (void) mapView:(GMSMapView *) mapView didTapInfoWindowOfMarker:(GMSMarker *)marker
