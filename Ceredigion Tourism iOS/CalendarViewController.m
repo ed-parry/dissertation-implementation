87b25c6d8cd1b9f3a7c0d7b71b72dc0332ea1adf
Ed Parry <me@edparry.com>
2014-03-10 17:26:40 +0000

    Spent most of the day trying to find a suitable calendar to use in the application. Little luck so far.

---
+++ Ceredigion Tourism iOS/CalendarViewController.m
@@ -0,0 +1,37 @@
+//
+//  CalendarViewController.m
+//  Ceredigion Tourism iOS
+//
+//  Created by Ed Parry on 10/03/2014.
+//  Copyright (c) 2014 Aberystwyth University. All rights reserved.
+//
+
+#import "CalendarViewController.h"
+
+@interface CalendarViewController ()
+
+@end
+
+@implementation CalendarViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    self.navigationController.navigationBar.translucent = NO;
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+@end
