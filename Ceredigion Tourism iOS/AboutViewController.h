f13e1a33b649af289a6c9839293b93c1790589ea
Ed Parry <me@edparry.com>
2014-03-18 14:38:56 +0000

    Added an About View Controller and imported in the open source licensing information for Google Maps iOS SDK.

---
+++ Ceredigion Tourism iOS/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  Ceredigion Tourism iOS
+//
+//  Created by Ed Parry on 18/03/2014.
+//  Copyright (c) 2014 Aberystwyth University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
---
+++ Ceredigion Tourism iOS/AboutViewController.m
@@ -0,0 +1,58 @@
+//
+//  AboutViewController.m
+//  Ceredigion Tourism iOS
+//
+//  Created by Ed Parry on 18/03/2014.
+//  Copyright (c) 2014 Aberystwyth University. All rights reserved.
+//
+
+#import "AboutViewController.h"
+#import <GoogleMaps/GoogleMaps.h>
+
+@interface AboutViewController ()
+@property (strong, nonatomic) IBOutlet UITextView *textView;
+
+- (IBAction)legalNoticesButtonTapped:(id)sender;
+@end
+
+@implementation AboutViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)legalNoticesButtonTapped:(id)sender
+{
+    _textView.text = [GMSServices openSourceLicenseInfo];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+
+@end
