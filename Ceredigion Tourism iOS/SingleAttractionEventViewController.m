d96fe3ddedd265b927ded199a70382f42bb1cf98
Ed Parry <me@edparry.com>
2014-03-24 18:09:10 +0000

    Something isn't quite right with the recent changes made. Let's roll back and try it again in the morning.

--- Ceredigion Tourism iOS/SingleAttractionEventViewController.m
+++ Ceredigion Tourism iOS/SingleAttractionEventViewController.m
@@ -8,6 +8,7 @@
 
 #import "SingleAttractionEventViewController.h"
 #import "DateFormatManager.h"
+#import <GoogleMaps/GoogleMaps.h>
 #import <EventKitUI/EventKitUI.h>
 
 @interface SingleAttractionEventViewController () <EKEventEditViewDelegate>
@@ -25,8 +26,7 @@
 // These string variables are used to link either an
 // Attraction or Event to the View itself.
 @property bool isAttraction;
-@property (strong, nonatomic) IBOutlet UIView *imageLoadingView;
-@property (strong, nonatomic) IBOutlet UIActivityIndicatorView *imageLoadingSpinner;
+
 
 @property (strong, nonatomic) NSString *firstTextFieldContent;
 @property (strong, nonatomic) NSString *secondTextFieldContent;
@@ -36,11 +36,17 @@
 @property (strong, nonatomic) NSString *thisWebsite;
 @property (strong, nonatomic) NSString *thisImageURL;
 @property UIImage *attractionImage;
+// Used to display a small map of the location
+@property (strong, nonatomic) NSString *thisLatitude;
+@property (strong, nonatomic) NSString *thisLongitude;
 
 // the start date is already in "secondTextFieldContent"
 @property (strong, nonatomic) NSString *eventEndDate;
 
 @property (strong, nonatomic) IBOutlet UIImageView *attractionImageView;
+@property (strong, nonatomic) IBOutlet UIView *imageLoadingView;
+@property (strong, nonatomic) IBOutlet UIView *smallMapView;
+@property (strong, nonatomic) IBOutlet UIActivityIndicatorView *imageLoadingSpinner;
 
 - (IBAction)phoneNumberClicked:(UIButton *)sender;
 - (IBAction)addToCalendarTapped:(id)sender;
@@ -59,10 +65,6 @@
     _thirdTextFieldContent = currentAttraction.descriptionText;
     _thisWebsite = currentAttraction.website;
     _thisImageURL = currentAttraction.imageLocationURL;
-    
-    _attractionImageView.hidden = YES;
-    _imageLoadingView.hidden = NO;
-    [_imageLoadingSpinner startAnimating];
 }
 
 - (void)startWithEvent:(Event *)currentEvent
@@ -83,6 +85,8 @@
     [self.navigationController.navigationBar setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:
                                                                      [UIColor whiteColor], NSForegroundColorAttributeName,[UIFont fontWithName:@"Avenir-Medium" size:18.0],
                                                                      NSFontAttributeName, nil]];
+    _imageLoadingView.hidden = NO;
+    _attractionImageView.hidden = YES;
     [_imageLoadingSpinner startAnimating];
     [self setUpViewContent];
 }
@@ -138,10 +142,7 @@
             [self performSelectorOnMainThread:@selector(putImageOnView) withObject:nil waitUntilDone:NO];
         }
         else{
-            [_imageLoadingSpinner stopAnimating];
-            _imageLoadingView.hidden = YES;
-            _attractionImageView.hidden = NO;
-            [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+            [self setUpMapView];
         }
 
         [self setPageColorForGroup:_thisGroup];
@@ -158,6 +159,22 @@
         // we now use this to store the start date and time, so shouldn't be a button.
         _secondTextField.enabled = NO;
     }
+}
+
+- (void)setUpMapView
+{
+    NSLog(@"Called");
+    [_imageLoadingSpinner stopAnimating];
+    _imageLoadingView.hidden = YES;
+    _attractionImageView.hidden = YES;
+    
+    double lat = [_thisLatitude doubleValue];
+    double longitude = [_thisLongitude doubleValue];
+
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:lat longitude:longitude zoom:12];
+    _smallMapView = [GMSMapView mapWithFrame:CGRectZero camera:camera];
+    
+    _smallMapView.hidden = NO;
 }
 
 - (NSString *)returnTextualDateTime:(NSString *)datetime
@@ -272,6 +289,7 @@
     _attractionImageView.contentMode = UIViewContentModeScaleToFill;
     [_imageLoadingSpinner stopAnimating];
     _imageLoadingView.hidden = YES;
+    _smallMapView.hidden = YES;
     _attractionImageView.hidden = NO;
     [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
 }
