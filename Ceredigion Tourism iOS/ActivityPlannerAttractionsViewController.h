29c0586578d9d51f8323f1de3c405bbf2eaba802
Ed Parry <me@edparry.com>
2014-03-25 19:33:21 +0000

    Added in a new ActivityPlannerAttractionsViewController file for the second view in the Activity Planner.

---
+++ Ceredigion Tourism iOS/ActivityPlannerAttractionsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ActivityPlannerAttractionsViewController.h
+//  Ceredigion Tourism iOS
+//
+//  Created by Ed Parry on 25/03/2014.
+//  Copyright (c) 2014 Aberystwyth University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActivityPlannerAttractionsViewController : UIViewController
+
+@end
---
+++ Ceredigion Tourism iOS/ActivityPlannerAttractionsViewController.m
@@ -0,0 +1,43 @@
+//
+//  ActivityPlannerAttractionsViewController.m
+//  Ceredigion Tourism iOS
+//
+//  Created by Ed Parry on 25/03/2014.
+//  Copyright (c) 2014 Aberystwyth University. All rights reserved.
+//
+
+#import "ActivityPlannerAttractionsViewController.h"
+
+@interface ActivityPlannerAttractionsViewController ()
+
+@end
+
+@implementation ActivityPlannerAttractionsViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self.view setBackgroundColor:[UIColor colorWithRed:(35.0/256.0)
+                                                  green:(164.0/256.0)
+                                                   blue:(219.0/256.0)
+                                                  alpha:(1.0)]];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
