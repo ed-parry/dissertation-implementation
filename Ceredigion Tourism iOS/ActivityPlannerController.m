0c06a8ef69220ac4786619eb103e9fcaa4ed08bd
Ed Parry <me@edparry.com>
2014-03-27 14:43:36 +0000

    Working on implementing the ActivityPlannerController functionality. About half way through, without the Adrenaline levels.

--- Ceredigion Tourism iOS/ActivityPlannerController.m
+++ Ceredigion Tourism iOS/ActivityPlannerController.m
@@ -7,9 +7,14 @@
 //
 
 #import "ActivityPlannerController.h"
+#import "CoreDataManager.h"
+#import "MapDataManager.h"
+#import "Attraction.h"
 
 @interface ActivityPlannerController ()
 @property ActivityPlan *thisPlan;
+@property CoreDataManager *coreDataManager;
+@property NSArray *allAttractionsInGroupArrays;
 @end
 
 @implementation ActivityPlannerController
@@ -19,21 +24,77 @@
     if(self){
         _thisPlan = [[ActivityPlan alloc] init];
         _thisPlan = plan;
+        
+        _coreDataManager = [[CoreDataManager alloc] init];
     }
-    
     return self;
 }
 
 - (NSArray *)generateActivityList
 {
     NSMutableArray *activityList = [[NSMutableArray alloc] init];
+    _allAttractionsInGroupArrays = [_coreDataManager getAllAttractionsInGroupArrays];
     
-    // need to figure out how to work this...should we fetch all activities/attractions, and then process in here? Or just grab what we want from Core Data from that manager?
+    NSArray *activityListForLocation = [[NSArray alloc] initWithArray:[self generateAttractionsForLocation:_thisPlan.locationCoordinates]];
+
     
-    // EP - think from here.
     
+    // Things to check for:
+    // * DONE location (10 miles of coordinates)
+    // * activity group // done automatically, when fetching X number of each group
+    // * adrenaline level
+    // * any events that match location and within days
+    // * total number
+    
+    
+    int totalActivities = [_thisPlan.numberOfActivities intValue];
+    int totalGroups = [_thisPlan.selectedGroups count];
+    
+    int activitiesPerGroup = totalActivities / totalGroups;
+    
+    for(NSString *group in _thisPlan.selectedGroups){
+        // this adds to the activityList array a collection of objects that are:
+        //  - within 10 miles of their chosen location
+        //  - are the correct number of activities, for each of their chosen groups
+        [activityList addObjectsFromArray:[self getNumberOfAttractions:activitiesPerGroup ofGroup:group usingActivityArray:activityListForLocation]];
+
+    }
+    
+
     return activityList;
 }
 
+- (NSArray *)generateSuitableEvents
+{
+    NSMutableArray *suitableEvents = [[NSMutableArray alloc] init];
+    // called by generateActivityList, to include any suitable events
+    return suitableEvents;
+}
+
+- (NSArray *)getNumberOfAttractions:(int)number ofGroup:(NSString *)group usingActivityArray:(NSArray *)activitiesArray
+{
+    NSMutableArray *attractionsForThisGroup = [[NSMutableArray alloc] init];
+    
+    return attractionsForThisGroup;
+}
+
+// Add's all attractions within 10 miles of the plan's location.
+- (NSArray *)generateAttractionsForLocation:(CLLocationCoordinate2D)location
+{
+    NSMutableArray *attractionsInLocation = [[NSMutableArray alloc] init];
+    
+    MapDataManager *mapManager = [[MapDataManager alloc] initWithCurrentRadiusCenter:location
+                                                                   andRadiusInMeters:16093.44];
+    for(NSArray *group in _allAttractionsInGroupArrays){
+        for(Attraction *tempAttraction in group){
+            CLLocationCoordinate2D attractionCoordinates = CLLocationCoordinate2DMake([tempAttraction.latitude doubleValue], [tempAttraction.longitude doubleValue]);
+            
+            if([mapManager isCoordinatesWithinRadius:attractionCoordinates]){
+                [attractionsInLocation addObject:tempAttraction];
+            }
+        }
+    }
+    return attractionsInLocation;
+}
 
 @end
