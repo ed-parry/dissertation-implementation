a00d9a424d1af4a7d9f853cf4209c77a58f1b352
Ed Parry <me@edparry.com>
2014-05-04 18:15:41 +0100

    More commenting.

--- Ceredigion Tourism iOS/AttractionPlannerResultsViewController.m
+++ Ceredigion Tourism iOS/AttractionPlannerResultsViewController.m
@@ -25,7 +25,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
     _activityResultsTableView.delegate = self;
     _activityResultsTableView.contentInset = UIEdgeInsetsZero;
     _activityResultsTableView.dataSource = self;
